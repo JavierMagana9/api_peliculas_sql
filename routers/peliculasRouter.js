a8eef9ce1df17fcd9b86b18054c947a088d0f7d9
JavierMagana9 <javier.magana9@gmail.com>
2024-01-30 21:42:12 +0100

    done: middleware folder and validator for creating movies

--- routers/peliculasRouter.js
+++ routers/peliculasRouter.js
@@ -1,15 +1,24 @@
 const express = require('express')
+const {check} = require('express-validator')
 const router = express.Router()
 
 const {getPeliculas,crearPelicula, actualizarPelicula,buscarPeliPorTitulo} = require('../controllers/peliculasController')
-
+const {validarCrearPelicula} = require('../middleware/validarPeliculas')
 
 //mostrar todos
 router.get('/movies', getPeliculas)
 //mostrar una pelicula
 router.get('/search/:title', buscarPeliPorTitulo)
-//crear
-router.post('/createMovie', crearPelicula)
+//crear titulo,anio,director, genero,duracion, imagen
+router.post('/createMovie', [
+    check('titulo', 'Por favor, introduce un titulo').not().isEmpty().trim(),
+    check('anio', 'Por favor, introduce un año desde 1900 hasta el año actual').not().isEmpty().isInt({ min: 1900, max: new Date().getFullYear() }),
+    check('director', 'Por favor, introduce el nombre de un director').not().isEmpty().trim(),
+    check('genero', 'Por favor, introduce un género').not().isEmpty().trim(),
+    check('imagen', "Por favor, introduce una URL válida").not().isEmpty().isURL(),
+    validarCrearPelicula
+],
+crearPelicula)
 //actualizar
 router.put('/editMovie/:id', actualizarPelicula)
 
