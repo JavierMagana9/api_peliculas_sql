83f48efba291e0f1d6396849200f9eb7bea053ea
jorgechavez99 <jorgeluischavez.jlcm@gmail.com>
2024-01-31 22:01:02 +0100

    DONE: Base de datos conectada atraves de render

--- configs/pgConfig.js
+++ configs/pgConfig.js
@@ -1,12 +1,21 @@
 
 const { Pool } = require('pg')
 //Necesitamos instancias el objeto Poll para poder utilizar y conectar con nuestra base de datos, la cual se encuentra en Postgres y necesitamos darle las conexiones como host,user,database y password
-
+// Se utiliza para administrar las conexiones con una base de datos PostgresSQL.
+//Actua como un reservorio de conexiones de cleinte que se puede reutilizar para solicitudes posteriores.
 const pool = new Pool({
-    host: process.env.HOST,
-    user: process.env.USERNAMEPG,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD
+
+    //La propiedad connectionString especifica los detalles de conexion para la base de datos Postgres
+    //CONNECTTRENDER es la variable de entorno en la cual nosotros atraves de un URL recogemos el host,user,password
+
+    connectionString:process.env.CONNECTRENDER,
+    
+// tenemos que utilizar SSL para indicar que la conexion encriptada es segura y pueda conectarse, garantizando la seguridad entre los intercambios de informacion que tendremos con la base de datos
+    ssl:true
+
+    // user: process.env.USERNAMEPG,
+    // database: process.env.DATABASE,
+    // password: process.env.PASSWORD
 })
 
 module.exports=pool
